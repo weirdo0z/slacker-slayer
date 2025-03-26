8c9165b90ee5bac03d16bd99744d3a826fe0667f
HideakiShirozu <hide_shiroozu@yahoo.co.jp>
2025-03-26 19:43:11 +0900

    feat: something

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 mod util;
 
 use anyhow::Context as _;
+use once_cell::sync::OnceCell;
 use serenity::all::{
-    ActivityData, CommandOptionType, CreateEmbed, CreateEmbedAuthor, GatewayIntents, GuildId,
-    Interaction, Mention, OnlineStatus,
+    ActivityData, CommandDataOptionValue, CommandInteraction, CommandOptionType, CreateEmbed,
+    CreateEmbedAuthor, CreateMessage, GatewayIntents, GuildId, Interaction, Mention, OnlineStatus,
+    UserId,
 };
 use serenity::async_trait;
 use serenity::builder::{
@@ -16,16 +18,36 @@ use std::sync::Arc;
 use tokio::sync::RwLock;
 use tracing::info;
 
-struct Bot {
-    discord_guild_id: GuildId,
-    ctx: Arc<RwLock<Option<Context>>>,
+static BOT: OnceCell<Arc<Bot>> = OnceCell::new();
+
+#[derive(Clone)]
+pub struct Bot {
+    pub discord_guild_id: GuildId,
+    pub ctx: Arc<RwLock<Option<Context>>>,
+    pub bot_id: UserId,
+}
+
+impl Bot {
+    pub async fn get_context() -> anyhow::Result<Context> {
+        let bot = BOT
+            .get()
+            .ok_or_else(|| anyhow::anyhow!("Bot not initialized"))?
+            .ctx
+            .read()
+            .await;
+        let ctx = bot
+            .as_ref()
+            .ok_or_else(|| anyhow::anyhow!("Context not available"))?;
+        Ok(ctx.to_owned())
+    }
 }
 
 #[async_trait]
 impl EventHandler for Bot {
     async fn ready(&self, ctx: Context, ready: Ready) {
         // Store context when bot is ready
-        *self.ctx.write().await = Some(ctx.clone());
+        *self.ctx.write().await = Some(ctx.to_owned());
+        BOT.get_or_init(|| Arc::new(self.to_owned()));
         info!("{} is connected!", ready.user.name);
         ctx.set_presence(
             Some(ActivityData::custom("*BGM of The Terminator*")),
@@ -57,6 +79,25 @@ impl EventHandler for Bot {
                     CreateCommandOption::new(CommandOptionType::User, "user", "User to remove")
                         .required(true),
                 ),
+            CreateCommand::new("assign")
+                .description("Assign a task to a team member")
+                .add_option(
+                    CreateCommandOption::new(CommandOptionType::User, "user", "User to assign")
+                        .required(true),
+                )
+                .add_option(
+                    CreateCommandOption::new(CommandOptionType::String, "task", "Task to assign")
+                        .required(true),
+                )
+                .add_option(
+                    CreateCommandOption::new(
+                        CommandOptionType::String,
+                        "deadline",
+                        "Deadline of the task (e.g., 'March 14th 16:32', 'tomorrow 3 am')",
+                    )
+                    .required(true),
+                ),
+            CreateCommand::new("members").description("Show the list of team members"),
             CreateCommand::new("progress")
                 .description("Report the progress")
                 .add_option(
@@ -77,12 +118,30 @@ impl EventHandler for Bot {
 
         info!(
             "Registered commands: {:#?}",
-            commands.iter().map(|c| c.name.clone()).collect::<Vec<_>>()
+            commands
+                .iter()
+                .map(|c| c.name.to_owned())
+                .collect::<Vec<_>>()
         );
     }
 
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::Command(command) = &interaction {
+            fn get_option(command: CommandInteraction, name: &str) -> CommandDataOptionValue {
+                command
+                    .data
+                    .options
+                    .iter()
+                    .find(|opt| opt.name == name)
+                    .cloned()
+                    .unwrap()
+                    .value
+            }
+
+            fn create_project_members_embed() -> CreateEmbed {
+                CreateEmbed::new().title("Members").description("members")
+            }
+
             let data = match command.data.name.as_str() {
                 "ぬるぽ" => CreateInteractionResponseMessage::new().content(
                     "
@@ -93,64 +152,89 @@ impl EventHandler for Bot {
 　 ＿/し'　／／. Ｖ｀Д´）/ ←お前
 　（＿フ彡　　　　　　/",
                 ),
-                "config" => CreateInteractionResponseMessage::new()
-                    .embed(
-                        CreateEmbed::new()
-                            .author(CreateEmbedAuthor::new("Slacker Slayer Config"))
-                            .description("Slacker Slayer GUI config"),
-                    )
-                    .ephemeral(true),
-                "import-config" => CreateInteractionResponseMessage::new()
-                    .content({
-                        let argument = command
-                            .data
-                            .options
-                            .iter()
-                            .find(|opt| opt.name == "config")
-                            .cloned();
-                        let value = argument.unwrap().value;
+                "config" => CreateInteractionResponseMessage::new().embed({
+                    CreateEmbed::new()
+                        .author(CreateEmbedAuthor::new("Slacker Slayer Config"))
+                        .description("Slacker Slayer GUI config")
+                }),
+                "import-config" => CreateInteractionResponseMessage::new().embed({
+                    let value = get_option(command.to_owned(), "config");
 
-                        let guild_id = &interaction.as_command().unwrap().guild_id.unwrap();
-                        util::import_config(*guild_id, value.clone()).await;
+                    let guild_id = &interaction.as_command().unwrap().guild_id.unwrap();
+                    util::import_config(*guild_id, value.to_owned()).await;
 
-                        format!("Imported setting from \"{}\"", value.as_str().unwrap())
-                    })
-                    .ephemeral(true),
+                    CreateEmbed::new()
+                        .author(CreateEmbedAuthor::new("Slacker Slayer Config"))
+                        .description(format!(
+                            "Imported \"{}\" and now the setting is:",
+                            value.as_str().unwrap(),
+                        ))
+                }),
                 "add" => CreateInteractionResponseMessage::new()
                     .content({
-                        let argument = command
-                            .data
-                            .options
-                            .iter()
-                            .find(|opt| opt.name == "user")
-                            .cloned();
-                        let value = argument.unwrap().value.as_user_id().unwrap();
+                        let value = get_option(command.to_owned(), "user");
+                        let user = value.as_user_id().unwrap();
 
-                        format!("Add: {}", Mention::from(value))
+                        format!("Added {}.", Mention::from(user))
                     })
-                    .ephemeral(true),
+                    .embed(create_project_members_embed()),
                 "remove" => CreateInteractionResponseMessage::new()
                     .content({
-                        let argument = command
-                            .data
-                            .options
-                            .iter()
-                            .find(|opt| opt.name == "user")
-                            .cloned();
-                        let value = argument.unwrap().value;
+                        let value = get_option(command.to_owned(), "user");
+                        let user = value.as_user_id().unwrap();
 
-                        format!("Remove: {:?}", value)
+                        format!("Removed {}.", Mention::from(user))
                     })
-                    .ephemeral(true),
+                    .embed(create_project_members_embed()),
+                "assign" => CreateInteractionResponseMessage::new().content({
+                    let (user, task, deadline) = (
+                        get_option(command.to_owned(), "user"),
+                        get_option(command.to_owned(), "task"),
+                        get_option(command.to_owned(), "deadline"),
+                    );
+
+                    format!(
+                        "Assigned {} to {} by {}.",
+                        Mention::from(user.as_user_id().unwrap()),
+                        task.as_str().unwrap(),
+                        deadline.as_str().unwrap()
+                    )
+                }),
+                "members" => {
+                    /*let guild_id = &interaction.as_command().unwrap().guild_id.unwrap();
+                    let members = util::get_project_members(ctx.to_owned(), *guild_id, bot_id).await;
+
+                    println!("{:?}", &members);
+
+                    CreateInteractionResponseMessage::new()
+                        .embed({
+                            match &members {
+                                Ok(members) => CreateEmbed::new()
+                                    .title("Current project members")
+                                    .description(
+                                        members
+                                            .iter()
+                                            .map(|user| {
+                                                if let Some(avatar) = &user.avatar_url {
+                                                    format!("[{}]({})", user.name, avatar)
+                                                } else {
+                                                    user.name.to_owned()
+                                                }
+                                            })
+                                            .collect::<Vec<String>>()
+                                            .join("\n"),
+                                    ),
+                                Err(_) => CreateEmbed::new()
+                                    .title("Oops,")
+                                    .description("member not found."),
+                            }
+                        })
+                        .ephemeral(members.is_err())*/
+                    CreateInteractionResponseMessage::new().embed(create_project_members_embed())
+                }
                 "progress" => CreateInteractionResponseMessage::new()
                     .content({
-                        let argument = command
-                            .data
-                            .options
-                            .iter()
-                            .find(|opt| opt.name == "progress")
-                            .cloned();
-                        let value = argument.unwrap().value;
+                        let value = get_option(command.to_owned(), "progress");
 
                         format!("You reported the progress: {}", value.as_str().unwrap())
                     })
@@ -180,7 +264,7 @@ async fn hourly_deadline_check(wrapped_ctx: Arc<RwLock<Option<Context>>>) {
         tokio::time::sleep(tokio::time::Duration::from_secs(3600)).await;
 
         let ctx = wrapped_ctx.read().await.as_ref().unwrap().to_owned();
-        let guild_ids = match util::get_guilds(ctx).await {
+        let guild_ids = match util::get_guilds().await {
             Ok(ids) => ids,
             Err(e) => {
                 println!("Failed to get guilds: {}", e);
@@ -189,9 +273,14 @@ async fn hourly_deadline_check(wrapped_ctx: Arc<RwLock<Option<Context>>>) {
         };
 
         for guild_id in guild_ids {
-            if let Ok(config) = util::read_config(guild_id).await {
-                println!("Guild {}: Config {:?}", guild_id, config);
-            }
+            let config = util::parse_config_code(
+                util::get_config_code(guild_id, ctx.http.get_current_user().await.unwrap().id)
+                    .await
+                    .unwrap(),
+            )
+            .await;
+
+            println!("Guild {}: Config {:?}", guild_id, config);
         }
     }
 }
@@ -208,6 +297,10 @@ async fn serenity(
         .get("DISCORD_GUILD_ID")
         .context("'DISCORD_GUILD_ID' was not found")?;
 
+    let bot_id = secret_store
+        .get("BOT_ID")
+        .context("'BOT_ID' was not found")?;
+
     // Create shared context
     let shared_ctx = Arc::new(RwLock::new(None));
 
@@ -216,7 +309,8 @@ async fn serenity(
         let intents = GatewayIntents::GUILDS;
         let bot = Bot {
             discord_guild_id: GuildId::new(discord_guild_id.parse().unwrap()),
-            ctx: shared_ctx.clone(),
+            ctx: shared_ctx.to_owned(),
+            bot_id: UserId::new(bot_id.parse().unwrap()),
         };
 
         Client::builder(&discord_token, intents)
--- src/util.rs
+++ src/util.rs
@@ -1,24 +1,27 @@
-use serenity::all::{CommandDataOptionValue, Context, GuildId, GuildPagination};
+use serenity::{
+    all::{ChannelId, CommandDataOptionValue, GuildId, GuildPagination, Message, UserId},
+    builder::GetMessages,
+};
 
 #[derive(Debug)]
 pub struct Config {
-    interval: String,
-    time: String,
+    pub interval: String,    // daily, weekly, monthly
+    pub time: u8,            // 0 to 23
+    pub weekday: Option<u8>, // 1-7 for Monday-Sunday
+    pub day: Option<u8>,     // 1-31 for day of month
+    pub channel_id: Option<ChannelId>,
 }
 
-pub async fn import_config(guild_id: GuildId, value: CommandDataOptionValue) {
-    println!("guild id {} value {}", guild_id, value.as_str().unwrap())
+#[derive(Debug)]
+pub struct Users {
+    pub id: UserId,
+    pub name: String,
+    pub avatar_url: Option<String>,
 }
 
-pub async fn read_config(_guild_id: GuildId) -> anyhow::Result<Config> {
-    // For now, return dummy values regardless of guild_id
-    Ok(Config {
-        interval: "daily".to_string(),
-        time: "09:00".to_string(),
-    })
-}
+pub async fn get_guilds() -> anyhow::Result<Vec<GuildId>> {
+    let ctx = crate::Bot::get_context().await?;
 
-pub async fn get_guilds(ctx: Context) -> anyhow::Result<Vec<GuildId>> {
     let mut last_guild_id = GuildId::new(1);
     let mut guild_ids: Vec<GuildId> = vec![];
 
@@ -44,3 +47,139 @@ pub async fn get_guilds(ctx: Context) -> anyhow::Result<Vec<GuildId>> {
 
     Ok(guild_ids)
 }
+
+pub async fn get_bot_messages(guild_id: GuildId) -> anyhow::Result<Vec<Message>> {
+    // Get the context from the global state
+    let ctx = crate::Bot::get_context().await?;
+    let bot_id = ctx.http.get_current_user().await.unwrap().id;
+
+    // Get all channels in the guild
+    let channels = guild_id.channels(&ctx).await?;
+
+    let mut bot_messages = Vec::new();
+
+    // Search through all channels for messages from the bot
+    for (_channel_id, channel) in channels {
+        // Get messages in the channel
+        let messages = channel
+            .messages(&ctx, GetMessages::new().limit(100))
+            .await?;
+
+        // Filter messages by author ID
+        for msg in messages {
+            if msg.author.id == bot_id {
+                bot_messages.push(msg);
+            }
+        }
+    }
+
+    println!("{:?}", bot_messages);
+
+    Ok(bot_messages)
+}
+
+pub async fn get_config_code(guild_id: GuildId, bot_id: UserId) -> anyhow::Result<String> {
+    let messages = get_bot_messages(guild_id).await.unwrap();
+
+    // Look for messages with embeds titled "Config"
+    for msg in messages {
+        for embed in msg.embeds {
+            if let Some(title) = &embed.title {
+                if title == "Config" && msg.author.id == bot_id {
+                    // For now, just print the config code
+                    println!("Found config message: {:?}", embed);
+
+                    if let Some(description) = &embed.description {
+                        return Ok(description.to_string());
+                    }
+                }
+            }
+        }
+    }
+
+    // If we get here, no config was found
+    println!("Config not found.");
+    anyhow::bail!("Config not found")
+}
+
+/* async fn get_system_channel(guild_id: GuildId) -> anyhow::Result<ChannelId> {
+    let ctx = crate::Bot::get_context().await?;
+
+    let guild = guild_id.to_partial_guild(&ctx.http).await?;
+    let system_channel = guild
+        .system_channel_id
+        .ok_or_else(|| anyhow::anyhow!("System channel not found for guild {}", guild_id))?;
+
+    Ok(system_channel)
+}
+
+pub async fn get_project_members(ctx: Context, guild_id: GuildId, bot_id: UserId) -> anyhow::Result<Vec<Users>> {
+    const config = parse_config_code(read_config_code(guild_id, bot_id));
+}
+
+pub async fn get_guild_members(guild_id: GuildId) -> anyhow::Result<Vec<Users>> {
+    let ctx = crate::Bot::get_context().await?;
+
+    let members = guild_id.members(&ctx.http, None, None).await?;
+
+    println!("members: {:?}", members);
+
+    let users = members
+        .into_iter()
+        .map(|member| Users {
+            id: member.user.id,
+            name: member.user.name.clone(),
+            avatar_url: member.user.avatar_url(),
+        })
+        .collect();
+
+    Ok(users)
+} */
+
+pub async fn import_config(guild_id: GuildId, value: CommandDataOptionValue) {
+    println!("guild id {} value {}", guild_id, value.as_str().unwrap())
+}
+
+pub async fn parse_config_code(code: String) -> Config {
+    // Format:
+    // daily: "ssc.daily.09:00.channel_id"
+    // weekly: "ssc.weekly.1.09:00.channel_id" (1 = Monday)
+    // monthly: "ssc.monthly.15.09:00.channel_id" (15 = 15th day of month)
+    let parts: Vec<&str> = code.trim().split('.').collect();
+
+    let interval = parts[1].to_string();
+    if (parts.len() < 3 || parts[0] != "ssc")
+        || (interval != "daily" && parts.len() < 3)
+        || (interval != "weekly" && parts.len() < 4)
+        || (interval != "monthly" && parts.len() < 4)
+    {
+        // If invalid format, return default config
+        return Config {
+            interval: "daily".to_string(),
+            time: 9,
+            weekday: None,
+            day: None,
+            channel_id: None,
+        };
+    }
+
+    Config {
+        interval: parts[1].to_string(),
+        time: parts[2].parse().unwrap_or(9),
+        weekday: parts[3].parse().ok(),
+        day: parts[4].parse().ok(),
+        channel_id: parts.get(5).and_then(|s| s.parse().ok()),
+    }
+}
+
+/* pub async fn config_gui(guild_id: GuildId) -> anyhow::Result<()> {
+    // Get the context from the global state
+    let ctx = crate::Bot::get_context().await?;
+
+    guild_id.channels(http);
+
+    // Get the system channel to send message
+    if let Some(channel) = guild_id.system_channel(&ctx).await? {
+        channel.send_message(&ctx, CreateMessage::new())
+    }
+} */
